31a70ef321826c4b2d2b31bafe1ff2355d8feeea
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-05-02 13:38:54 +0100

    Add product and product image features

--- main/forms.py
+++ main/forms.py
@@ -4,6 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class ContactForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
     message = forms.CharField(max_length=600, widget=forms.Textarea)
