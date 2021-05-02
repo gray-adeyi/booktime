19f3cb230aa6320f52a2d6c59f8087950263d332
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-05-02 11:59:09 +0100

    Add booktime homepage and about us page

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path(
+        "about-us/",
+        TemplateView.as_view(template_name="about_us.html")
+    ),
+    path(
+        '',
+        TemplateView.as_view(template_name='home.html')
+    )
+]
