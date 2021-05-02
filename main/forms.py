872ee96863251777bc29ce83059c6badab7ab20f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-05-02 23:33:09 +0100

    Add login feature to booktime

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.forms import (
     UserCreationForm as DjangoUserCreationForm
 )
 from django.contrib.auth.forms import UsernameField
+from django.contrib.auth import authenticate
 from . import models
 
 
@@ -46,3 +47,34 @@ class ContactForm(forms.Form):
             ['customerservice@booktime.domain'],
             fail_silently=False
         )
+
+
+class AuthenticationForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(
+        strip=False, widget=forms.PasswordInput
+    )
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user = None
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        if email is not None and password:
+            self.user = authenticate(
+                self.request, email=email, password=password
+            )
+            if self.user is None:
+                raise forms.ValidationError(
+                    "Invalid email/password combination."
+                )
+            logger.info(
+                "Authentication successful for email=%s", email
+            )
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 from main import views
 from main import models
+from main import  forms
 
 urlpatterns = [
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            form_class=forms.AuthenticationForm,
+        ),
+        name="login",
+    ),
     path(
         "signup/",
         views.SignupView.as_view(),
